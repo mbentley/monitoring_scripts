2832f250da851f05aaca79509a422d060e0ef8ff
Matt Bentley <mbentley@mbentley.net>
2015-06-04 11:37:56 -0400

    updated scripts

--- check_process.sh
+++ check_process.sh
@@ -5,142 +5,142 @@ SHORT_PN=${1}
 LONG_PN=${2}
 RESTART=${3:-0}
 DOWN_FILE="down.${SHORT_PN}"
-EMAIL_TO=mbentley@mbentley.net
-EMAIL_FROM=mbentley@mbentley.net
+EMAIL_TO=youremail@yourdomain.com
+EMAIL_FROM=youremail@yourdomain.com
 RUN_COUNT=0
 SENDMAIL=/usr/sbin/sendmail
 
 function check_vars {
-	if [ -z "${SHORT_PN}" ]
-	then
-		echo -e "You must provide a short process name as the first parameter\nExample:  ${0} sshd /usr/sbin/sshd"
-		exit 1
-	fi
+  if [ -z "${SHORT_PN}" ]
+  then
+    echo -e "You must provide a short process name as the first parameter\nExample: ${0} sshd /usr/sbin/sshd"
+    exit 1
+  fi
 
-	if [ -z "${LONG_PN}" ]
-	then
-		echo -e "You must provide a long process name as the second parameter\nExample:  ${0} sshd /usr/sbin/sshd"
-		exit 1
-	else
-		temp_dir
-	fi
+  if [ -z "${LONG_PN}" ]
+  then
+    echo -e "You must provide a long process name as the second parameter\nExample: ${0} sshd /usr/sbin/sshd"
+    exit 1
+  else
+    temp_dir
+  fi
 }
 
 function temp_dir {
-	if [ ! -d "${CHECKPROC_DIR}" ]
-	then
-		mkdir ${CHECKPROC_DIR}
-	fi
+  if [ ! -d "${CHECKPROC_DIR}" ]
+  then
+    mkdir ${CHECKPROC_DIR}
+  fi
 
-	TEMPDIR=/tmp/checkproc_${SHORT_PN}_`date +%N`
-	mkdir ${TEMPDIR}
-	if [ ! -d "${TEMPDIR}" ]
-	then
-		echo "unable to create temp directory"
-		exit 1
-	fi
-	check_process
+  TEMPDIR=/tmp/checkproc_${SHORT_PN}_$(date +%N)
+  mkdir ${TEMPDIR}
+  if [ ! -d "${TEMPDIR}" ]
+  then
+    echo "unable to create temp directory"
+    exit 1
+  fi
+  check_process
 }
 
 function check_process {
-	RUN_COUNT=$(expr $RUN_COUNT + 1)
+  RUN_COUNT=$(expr $RUN_COUNT + 1)
 
-	if [ ${RUN_COUNT} -gt "3" ]; then
-		echo -e "check_process has ran more then 3 times.\naborting."
-		exit 1
-	fi
+  if [ ${RUN_COUNT} -gt "3" ]; then
+    echo -e "check_process has ran more then 3 times.\naborting."
+    exit 1
+  fi
 
-	if ps aux | grep "${LONG_PN}" | grep -v ${0} | grep -v grep  > /dev/null ; then
-	        echo "${SHORT_PN} is running."
-		online_check
-	else
-		echo -e "${SHORT_PN} may be down.\nperforming further tests..."
-		sleep 2
-		process_down_verify
-	fi
+  if ps aux | grep "${LONG_PN}" | grep -v ${0} | grep -v grep  > /dev/null ; then
+          echo "${SHORT_PN} is running."
+    online_check
+  else
+    echo -e "${SHORT_PN} may be down.\nperforming further tests..."
+    sleep 2
+    process_down_verify
+  fi
 }
 
 function process_down_verify {
-	DOWN_COUNT=0
+  DOWN_COUNT=0
 
-	for (( i=1; i<=5; i++ ))
-	do
-		sleep 2
+  for (( i=1; i<=5; i++ ))
+  do
+    sleep 2
 
-		if ps aux | grep "${LONG_PN}" | grep -v ${0} | grep -v grep  > /dev/null ; then
-		        echo "false alarm, ${SHORT_PN} is running."
-			i=5
-		else
-		        echo "check ${i}: ${SHORT_PN} is not running..."
-			DOWN_COUNT=$(expr $DOWN_COUNT + 1)
-		fi
-	done
+    if ps aux | grep "${LONG_PN}" | grep -v ${0} | grep -v grep  > /dev/null ; then
+            echo "false alarm, ${SHORT_PN} is running."
+      i=5
+    else
+            echo "check ${i}: ${SHORT_PN} is not running..."
+      DOWN_COUNT=$(expr $DOWN_COUNT + 1)
+    fi
+  done
 
-	if [ ${DOWN_COUNT} -eq "5" ]; then
-		echo "check_process has verified that ${SHORT_PN} is down."
-		email_offline
-	else
-		echo "re-starting check_process..."
-		check_process
-	fi
+  if [ ${DOWN_COUNT} -eq "5" ]; then
+    echo "check_process has verified that ${SHORT_PN} is down."
+    email_offline
+  else
+    echo "re-starting check_process..."
+    check_process
+  fi
 }
 
 function online_check {
-	if [ -f ${CHECKPROC_DIR}/${DOWN_FILE} ]
-	then
-		rm ${CHECKPROC_DIR}/${DOWN_FILE}
-		MAIL=${TEMPDIR}/mail_`date +%F`
-		touch ${MAIL}
-		chmod 600 ${MAIL}
-		echo "To: ${EMAIL_TO}" >> ${MAIL}
-		echo "From: `hostname --fqdn` <${EMAIL_FROM}>" >> ${MAIL}
-		echo "Subject: ${SHORT_PN} on `hostname` is back up" >> ${MAIL}
-		echo "" >> ${MAIL}
-		echo "check_process has determined that ${SHORT_PN} on `hostname` is back up." >> ${MAIL}
-		${SENDMAIL} -t -f ${EMAIL_TO} < ${MAIL}
-	fi
+  if [ -f ${CHECKPROC_DIR}/${DOWN_FILE} ]
+  then
+    rm ${CHECKPROC_DIR}/${DOWN_FILE}
+    MAIL=${TEMPDIR}/mail_$(date +%F)
+    touch ${MAIL}
+    chmod 600 ${MAIL}
+    echo "To: ${EMAIL_TO}" >> ${MAIL}
+    echo "From: $(hostname --fqdn) <${EMAIL_FROM}>" >> ${MAIL}
+    echo "Subject: ${SHORT_PN} on $(hostname) is back up" >> ${MAIL}
+    echo "" >> ${MAIL}
+    echo "check_process has determined that ${SHORT_PN} on $(hostname) is back up." >> ${MAIL}
+    ${SENDMAIL} -t < ${MAIL}
+  fi
 
-	cleanup_tmp
+  cleanup_tmp
 }
 
 function email_offline {
-	if [ ! -f ${CHECKPROC_DIR}/${DOWN_FILE} ]
-	then
-		MAIL=${TEMPDIR}/mail_`date +%F`
-		touch ${MAIL}
-		chmod 600 ${MAIL}
-		echo "To: ${EMAIL_TO}" >> ${MAIL}
-		echo "From: `hostname --fqdn` <${EMAIL_FROM}>" >> ${MAIL}
-		case ${RESTART} in
-			1|true)
-				echo "Subject: ${SHORT_PN} on `hostname` was down" >> ${MAIL}
-				echo "" >> ${MAIL}
-				echo "check_process has determined that ${SHORT_PN} on `hostname` was not running and has restarted it:" >> ${MAIL}
-				/etc/init.d/${SHORT_PN} restart >> ${MAIL} 2>&1
-				;;
-			*)
-				echo "Subject: ${SHORT_PN} on `hostname` is down" >> ${MAIL}
-				echo "" >> ${MAIL}
-				echo "check_process has determined that ${SHORT_PN} on `hostname` is not running." >> ${MAIL}
-				;;
-		esac
-		${SENDMAIL} -t -f ${EMAIL_TO} < ${MAIL}
-	fi
+  if [ ! -f ${CHECKPROC_DIR}/${DOWN_FILE} ]
+  then
+    MAIL=${TEMPDIR}/mail_$(date +%F)
+    touch ${MAIL}
+    chmod 600 ${MAIL}
+    echo "To: ${EMAIL_TO}" >> ${MAIL}
+    echo "From: $(hostname --fqdn) <${EMAIL_FROM}>" >> ${MAIL}
+    case ${RESTART} in
+      1|true)
+        echo "Subject: ${SHORT_PN} on $(hostname) was down" >> ${MAIL}
+        echo "" >> ${MAIL}
+        echo "check_process has determined that ${SHORT_PN} on $(hostname) was not running and has restarted it:" >> ${MAIL}
+        /etc/init.d/${SHORT_PN} restart >> ${MAIL} 2>&1
+        ;;
+      *)
+        echo "Subject: ${SHORT_PN} on $(hostname) is down" >> ${MAIL}
+        echo "" >> ${MAIL}
+        echo "check_process has determined that ${SHORT_PN} on $(hostname) is not running." >> ${MAIL}
+        ;;
+    esac
+    ${SENDMAIL} -t < ${MAIL}
+  fi
 
-	case ${RESTART} in
-		1|true)
-			cleanup_tmp
-			;;
-		*)
-			touch ${CHECKPROC_DIR}/${DOWN_FILE}
-			cleanup_tmp
-			;;
-	esac
+  case ${RESTART} in
+    1|true)
+      cleanup_tmp
+      ;;
+    *)
+      touch ${CHECKPROC_DIR}/${DOWN_FILE}
+      cleanup_tmp
+      ;;
+  esac
 }
 
 function cleanup_tmp {
-	rm -rf ${TEMPDIR}
-	exit 0
+  rm -rf ${TEMPDIR}
+  exit 0
 }
 
 check_vars
--- webmon.sh
+++ webmon.sh
@@ -4,127 +4,147 @@ WEBMON_DIR="/tmp/webmon"
 SHORT_URL=${1}
 FULL_URL=${2}
 DOWN_FILE="down.${SHORT_URL}"
-EMAIL_TO=mbentley@mbentley.net
-EMAIL_FROM=mbentley@mbentley.net
-LOG_FILE="/var/www/mbentley.net/neptune.mbentley.net/webmon_logs/webmon.${SHORT_URL}.txt"
+EMAIL_TO=youremail@yourdomain.com
+EMAIL_FROM=youremail@yourdomain.com
+LOG_FILE="/path/to/your/logfile/webmon.${SHORT_URL}.txt"
 OFFLINE_COUNT=0
 SENDMAIL=/usr/sbin/sendmail
 
 function check_vars {
-	if [ -z ${SHORT_URL} ]
-	then
-		echo -e "You must provide a short URL name as the first parameter\nExample:  ${0} google.com http://www.google.com/"
-		exit 1
-	fi
+  if [ -z ${SHORT_URL} ]
+  then
+    echo -e "You must provide a short URL name as the first parameter\nExample:  ${0} google.com http://www.google.com/"
+    exit 1
+  fi
 
-	if [ -z ${FULL_URL} ]
-	then
-		echo -e "You must provide a full URL as the second parameter\nExample:  ${0} google.com http://www.google.com/"
-		exit 1
-	else
-		temp_dir
-	fi
+  if [ -z ${FULL_URL} ]
+  then
+    echo -e "You must provide a full URL as the second parameter\nExample:  ${0} google.com http://www.google.com/"
+    exit 1
+  else
+    temp_dir
+  fi
 }
 
 function temp_dir {
-	if [ ! -d "${WEBMON_DIR}" ]
-	then
-		mkdir ${WEBMON_DIR}
-	fi
+  if [ ! -d "${WEBMON_DIR}" ]
+  then
+    mkdir ${WEBMON_DIR}
+  fi
 
-	if [ ! -f ${LOG_FILE} ]
-	then
-		echo -e "STATUS\tDATE/TIME\t\tSTATUS CODE" > ${LOG_FILE}
-	fi
+  if [ ! -f ${LOG_FILE} ]
+  then
+    echo -e "STATUS\tDATE/TIME\t\tSTATUS CODE" > ${LOG_FILE}
+  fi
 
-	TEMPDIR=/tmp/webmon_${SHORT_URL}_`date +%N`
-	mkdir ${TEMPDIR}
-	if [ ! -d "${TEMPDIR}" ]
-	then
-		echo "unable to create temp directory"
-		exit 1
-	fi
-	query_status
+  TEMPDIR=/tmp/webmon_${SHORT_URL}_$(date +%N)
+  mkdir ${TEMPDIR}
+  if [ ! -d "${TEMPDIR}" ]
+  then
+    echo "unable to create temp directory"
+    exit 1
+  fi
+  internet_check
+}
+
+function internet_check {
+  FULL_INET_STATUS_CODE=$(curl -q -k -m 5 -s -I http://www.google.com/robots.txt | grep HTTP)
+  SHORT_INET_STATUS_CODE=$(echo ${FULL_INET_STATUS_CODE} | awk '{ print $2 }')
+
+  if [ -z ${SHORT_INET_STATUS_CODE} ]
+  then
+    echo ${FULL_INET_STATUS_CODE}
+    echo "Unable to access Google; assuming offline"
+    exit 1
+  fi
+
+  if [ ${SHORT_INET_STATUS_CODE} != "200" ]
+  then
+    echo ${FULL_INET_STATUS_CODE}
+    echo "Unable to access Google; assuming offline"
+    exit 1
+  fi
+
+  query_status
 }
 
 function query_status {
-	FULL_STATUS_CODE=`curl -q -k -m 30 -s -I ${FULL_URL} | grep HTTP`
-	SHORT_STATUS_CODE=`echo ${FULL_STATUS_CODE} | awk '{ print $2 }'`
+  FULL_STATUS_CODE=$(curl -q -k -m 15 -s -I ${FULL_URL} | grep HTTP)
+  SHORT_STATUS_CODE=$(echo ${FULL_STATUS_CODE} | awk '{ print $2 }')
 
-	if [ -z ${SHORT_STATUS_CODE} ]
-	then
-		FULL_STATUS_CODE="No HTTP code returned"
-		verify_offline
-	fi
+  if [ -z ${SHORT_STATUS_CODE} ]
+  then
+    FULL_STATUS_CODE="No HTTP code returned"
+    verify_offline
+  fi
 
-	if [ ${SHORT_STATUS_CODE} != "200" ]
-	then
-		verify_offline
-	else
-		echo "${FULL_STATUS_CODE}"
-		online_check
-	fi
+  if [ ${SHORT_STATUS_CODE} != "200" ]
+  then
+    verify_offline
+  else
+    echo "${FULL_STATUS_CODE}"
+    online_check
+  fi
 }
 
 function verify_offline {
-	OFFLINE_COUNT=`expr ${OFFLINE_COUNT} + 1`
+  OFFLINE_COUNT=$(expr ${OFFLINE_COUNT} + 1)
 
-	if [ ${OFFLINE_COUNT} == 3 ]
-	then
-		echo "${FULL_STATUS_CODE}"
-		email_offline
-	else
-		sleep 1
-		query_status
-	fi
+  if [ ${OFFLINE_COUNT} == 2 ]
+  then
+    echo "${FULL_STATUS_CODE}"
+    email_offline
+  else
+    query_status
+  fi
 }
 
 function online_check {
-	if [ -f ${WEBMON_DIR}/${DOWN_FILE} ]
-	then
-		echo -e "UP\t`date +%F" "%R" "%Z`\t${FULL_STATUS_CODE}" >> ${LOG_FILE}
-		rm ${WEBMON_DIR}/${DOWN_FILE}
-		MAIL=${TEMPDIR}/mail_`date +%F`
-		touch ${MAIL}
-		chmod 600 ${MAIL}
-		echo "To: ${EMAIL_TO}" >> ${MAIL}
-		echo "From: `hostname --fqdn` <${EMAIL_FROM}>" >> ${MAIL}
-		echo "Subject: webmon: ${SHORT_URL} is back online" >> ${MAIL}
-		echo "" >> ${MAIL}
-		echo "webmon status:" >> ${MAIL}
-		echo "     ${SHORT_URL} (${FULL_URL}) is back online" >> ${MAIL}
-		echo "     ${FULL_STATUS_CODE}" >> ${MAIL}
-		${SENDMAIL} -t -f ${EMAIL_TO} < ${MAIL}
-	fi
+  if [ -f ${WEBMON_DIR}/${DOWN_FILE} ]
+  then
+    echo -e "UP\t$(date +%F" "%R" "%Z)\t${FULL_STATUS_CODE}" >> ${LOG_FILE}
+    rm ${WEBMON_DIR}/${DOWN_FILE}
+    MAIL=${TEMPDIR}/mail_$(date +%F)
+    touch ${MAIL}
+    chmod 600 ${MAIL}
+    echo "To: ${EMAIL_TO}" >> ${MAIL}
+    echo "From: $(hostname --fqdn) <${EMAIL_FROM}>" >> ${MAIL}
+    echo "Subject: webmon: ${SHORT_URL} is back online" >> ${MAIL}
+    echo "" >> ${MAIL}
+    echo "webmon status:" >> ${MAIL}
+    echo "     ${SHORT_URL} (${FULL_URL}) is back online" >> ${MAIL}
+    echo "     ${FULL_STATUS_CODE}" >> ${MAIL}
+    ${SENDMAIL} -t < ${MAIL}
+  fi
 
-	cleanup_tmp
+  cleanup_tmp
 }
 
 function email_offline {
-	if [ ! -f ${WEBMON_DIR}/${DOWN_FILE} ]
-	then
-		echo -e "DOWN\t`date +%F" "%R" "%Z`\t${FULL_STATUS_CODE}" >> ${LOG_FILE}
-		MAIL=${TEMPDIR}/mail_`date +%F`
-		touch ${MAIL}
-		chmod 600 ${MAIL}
-		echo "To: ${EMAIL_TO}" >> ${MAIL}
-		echo "From: `hostname --fqdn` <${EMAIL_FROM}>" >> ${MAIL}
-		echo "Subject: webmon: ${SHORT_URL} is offline" >> ${MAIL}
-		echo "" >> ${MAIL}
-		echo "webmon status:" >> ${MAIL}
-		echo "     ${SHORT_URL} (${FULL_URL}) is currently offline" >> ${MAIL}
-		echo "     ${FULL_STATUS_CODE}" >> ${MAIL}
-		${SENDMAIL} -t -f ${EMAIL_TO} < ${MAIL}
-	fi
+  if [ ! -f ${WEBMON_DIR}/${DOWN_FILE} ]
+  then
+    echo -e "DOWN\t$(date +%F" "%R" "%Z)\t${FULL_STATUS_CODE}" >> ${LOG_FILE}
+    MAIL=${TEMPDIR}/mail_$(date +%F)
+    touch ${MAIL}
+    chmod 600 ${MAIL}
+    echo "To: ${EMAIL_TO}" >> ${MAIL}
+    echo "From: $(hostname --fqdn) <${EMAIL_FROM}>" >> ${MAIL}
+    echo "Subject: webmon: ${SHORT_URL} is offline" >> ${MAIL}
+    echo "" >> ${MAIL}
+    echo "webmon status:" >> ${MAIL}
+    echo "     ${SHORT_URL} (${FULL_URL}) is currently offline" >> ${MAIL}
+    echo "     ${FULL_STATUS_CODE}" >> ${MAIL}
+    ${SENDMAIL} -t < ${MAIL}
+  fi
 
-	touch ${WEBMON_DIR}/${DOWN_FILE}
+  touch ${WEBMON_DIR}/${DOWN_FILE}
 
-	cleanup_tmp
+  cleanup_tmp
 }
 
 function cleanup_tmp {
-	rm -rf ${TEMPDIR}
-	exit 0
+  rm -rf ${TEMPDIR}
+  exit 0
 }
 
 check_vars
